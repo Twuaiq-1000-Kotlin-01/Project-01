474b3aa1788f7e1789b2ec5bd366bf73c03a2d7e
Ali <xinickelz@gmail.com>
2022-01-12 16:55:35 +0300

    Created .gitignore file, deleted unused packages and added image picker library

--- app/src/main/java/com/shahad/instic/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shahad/instic/ui/home/HomeFragment.kt
@@ -12,27 +12,15 @@ import com.shahad.instic.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        
+        return binding.root
     }
 }
