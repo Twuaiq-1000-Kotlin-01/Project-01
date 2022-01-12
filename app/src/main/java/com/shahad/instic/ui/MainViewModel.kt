4ea8b0aa6fdba7c184827475dfc8cb7c3e1ed8bb
Ali <xinickelz@gmail.com>
2022-01-12 19:08:46 +0300

    Added the ability to create posts, you can also attach an image with each post

--- app/src/main/java/com/shahad/instic/ui/MainViewModel.kt
+++ app/src/main/java/com/shahad/instic/ui/MainViewModel.kt
@@ -1,74 +1,91 @@
 package com.shahad.instic.ui
 
 import android.util.Log
+import androidx.core.net.toUri
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.*
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.storage.FirebaseStorage
 import com.shahad.instic.ui.model.Post
+import io.reactivex.Completable
+import io.reactivex.CompletableEmitter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.util.*
 
 class MainViewModel : ViewModel() {
-    private val auth = FirebaseAuth.getInstance()
-    private val db = FirebaseFirestore.getInstance()
-    private val uId = auth.currentUser!!.uid
-    private val collection = db.collection("post")
-    private var postsList = mutableListOf<Post>()
-
-    fun newPost(text: String, imagePath: String) {
-        val post = Post(uId, text, imagePath)
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                collection.document().set(post).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-
-                    } else {
-                        Log.e("newPost failed", task.exception?.message.toString())
-                    }
-                }
-
-            }
-        }
-    }
-
-
-    fun getPosts(): MutableLiveData<List<Post>> {
-        var livePostsList = MutableLiveData<List<Post>>()
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                collection.orderBy("date")
-                    .addSnapshotListener(object : EventListener<QuerySnapshot> {
-                        override fun onEvent(
-                            value: QuerySnapshot?,
-                            error: FirebaseFirestoreException?
-                        ) {
-
-
-                            if (error != null) {
-                                Log.e("getPosts failed", error.message.toString())
-                                return
-                            }
-
-                            for (dc: DocumentChange in value?.documentChanges!!) {
-                                if (dc.type == DocumentChange.Type.ADDED) {
-                                    postsList.add(dc.document.toObject(Post::class.java))
-                                }
-                            }
-                            livePostsList.postValue(postsList)
-
-
-                        }
-
-                    })
-
-
-            }
-        }
-        return livePostsList
-    }
-
-
+	private val auth = FirebaseAuth.getInstance()
+	private val db = FirebaseFirestore.getInstance()
+	private val uId = auth.currentUser!!.uid
+	private val collection = db.collection("post")
+	private var postsList = mutableListOf<Post>()
+	
+	fun newPost(text: String, imagePath: String?) = Completable.create { emitter ->
+		val randomUuid = UUID.randomUUID().toString()
+		val post = Post(uId, text, randomUuid)
+		viewModelScope.launch {
+			withContext(Dispatchers.IO) {
+				collection.document().set(post).addOnCompleteListener { task ->
+					if (task.isSuccessful) {
+						if (imagePath != null) {
+							uploadImage(imagePath, randomUuid, emitter)
+						} else {
+							emitter.onComplete()
+						}
+					} else {
+						Log.e("newPost failed", task.exception?.message.toString())
+					}
+				}
+			}
+		}
+	}
+	
+	private fun uploadImage(path: String, uuid: String, emitter: CompletableEmitter) {
+		val storage = FirebaseStorage.getInstance()
+		val storageRef = storage.reference
+		val imageRef = storageRef.child("uploads/$uuid")
+		val uri = File(path).toUri()
+		imageRef.putFile(uri)
+			.addOnSuccessListener {
+				Log.i("MainViewModel", "uploadImage: Uploaded $uuid")
+				emitter.onComplete()
+			}
+			.addOnFailureListener {
+				emitter.onError(it)
+				Log.e("MainViewModel", "uploadImage: ${it.message}", it)
+			}
+	}
+	
+	fun getPosts(): MutableLiveData<List<Post>> {
+		val livePostsList = MutableLiveData<List<Post>>()
+		viewModelScope.launch {
+			withContext(Dispatchers.IO) {
+				collection.orderBy("date")
+					.addSnapshotListener(object : EventListener<QuerySnapshot> {
+						override fun onEvent(
+							value: QuerySnapshot?,
+							error: FirebaseFirestoreException?
+						) {
+							if (error != null) {
+								Log.e("getPosts failed", error.message.toString())
+								return
+							}
+							
+							for (dc: DocumentChange in value?.documentChanges!!) {
+								if (dc.type == DocumentChange.Type.ADDED) {
+									postsList.add(dc.document.toObject(Post::class.java))
+								}
+							}
+							livePostsList.postValue(postsList)
+						}
+					})
+			}
+		}
+		return livePostsList
+	}
 }
--- app/src/main/java/com/shahad/instic/ui/home/PostFragment.kt
+++ app/src/main/java/com/shahad/instic/ui/home/PostFragment.kt
@@ -1,30 +1,101 @@
 package com.shahad.instic.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.shahad.instic.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.shahad.instic.databinding.FragmentPostBinding
+import com.shahad.instic.ui.MainViewModel
+import com.shahad.instic.ui.MediaLoader
+import com.yanzhenjie.album.Album
+import com.yanzhenjie.album.AlbumConfig
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class PostFragment : Fragment() {
-
-    private lateinit var binding: FragmentPostBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentPostBinding.inflate(inflater, container, false)
-        
-        
-        
-        return binding.root
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = PostFragment()
-    }
+	
+	private lateinit var binding: FragmentPostBinding
+	private var imagePath: String? = null
+	private val disposables = CompositeDisposable()
+	private val viewModel by activityViewModels<MainViewModel>()
+	
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		binding = FragmentPostBinding.inflate(inflater, container, false)
+		
+		initializeAlbumLib()
+		
+		binding.attachMediaButton.setOnClickListener {
+			Album.album(this)
+				.singleChoice()
+				.camera(true)
+				.onResult {
+					val file = it.first()
+					imagePath = file.path
+				}
+				.onCancel {
+					Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+				}
+				.start()
+		}
+		
+		binding.postButton.setOnClickListener {
+			val content = binding.postContentEditText.text.toString()
+			onPostInitiated()
+			
+			viewModel.newPost(content, imagePath)
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe({
+					onPostCreated()
+					Toast.makeText(activity, "Post created successfully!", Toast.LENGTH_SHORT)
+						.show()
+				}, { error ->
+					Toast.makeText(activity, error.message, Toast.LENGTH_SHORT).show()
+				}).also { disposable ->
+					disposables.add(disposable)
+				}
+		}
+		
+		return binding.root
+	}
+	
+	private fun initializeAlbumLib() {
+		Album.initialize(
+			AlbumConfig.newBuilder(requireContext())
+				.setAlbumLoader(MediaLoader())
+				.build()
+		)
+	}
+	
+	private fun onPostCreated() = with(binding) {
+		postButton.isEnabled = true
+		attachMediaButton.isEnabled = true
+		progressBar.visibility = View.GONE
+		postContentEditText.text.clear()
+		imagePath = null
+	}
+	
+	private fun onPostInitiated() = with(binding) {
+		postButton.isEnabled = false
+		attachMediaButton.isEnabled = false
+		postContentEditText.isEnabled = false
+		progressBar.visibility = View.VISIBLE
+	}
+	
+	override fun onStop() {
+		super.onStop()
+		disposables.clear()
+	}
+	
+	companion object {
+		@JvmStatic
+		fun newInstance() = PostFragment()
+	}
 }
