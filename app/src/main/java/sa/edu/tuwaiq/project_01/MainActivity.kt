a030ce90815905340bfb590031a396f8d9d06b1a
Ghadeer <gadeer.a094@gmail.com>
2022-01-12 14:27:32 +0300

    identity, and main package

--- app/src/main/java/sa/edu/tuwaiq/project_01/MainActivity.kt
+++ app/src/main/java/sa/edu/tuwaiq/project_01/MainActivity.kt
@@ -2,10 +2,22 @@ package sa.edu.tuwaiq.project_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sa.edu.tuwaiq.project_01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /* This line is to hide the shadow for the bottom nav view. The shadow will still appear
+          in the xml, but it will disappear when we run the app */
+        binding.bottomNavigationView.background = null
+
+        // This line is to disable the placeholder item in the bottom nav view. 1 -> placeholder item index
+        binding.bottomNavigationView.menu.getItem(1).isEnabled = false
+
+
     }
 }
